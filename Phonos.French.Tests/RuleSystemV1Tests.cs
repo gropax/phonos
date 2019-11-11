7cd0a2ad467b1be5d60788cf4babff03381ceede
mla <mla@proxem.com>
2019-11-11 18:44:14 +0100

    Add Rule test helper methods.

--- Phonos.French.Tests/RuleSystemV1Tests.cs
+++ Phonos.French.Tests/RuleSystemV1Tests.cs
@@ -20,18 +20,95 @@ namespace Phonos.French.Tests
             SyllableAnalyzer = new Latin.SyllableAnalyzer();
         }
 
-        [Fact]
-        public void TestRule1()
+        [Theory]
+        [InlineData("ambulat", /*phono:*/ "amblat", /*graphs:*/ "amblat")]
+        public void TestRule1(params string[] data)
         {
-            var latin = "ambulat";
-            var word = WordParser.Parse(latin);
+            TestRule(RuleSystem.Rule1(), data);
+        }
+
+
+        private void TestRule(Rule rule, string[] data)
+        {
+            var testData = ParseData(data);
+
+            var word = WordParser.Parse(testData.Latin);
             SyllableAnalyzer.Analyze(word);
 
-            var rule = RuleSystem.Rule1();
             var derived = rule.Apply(word);
 
-            Assert.Single(derived);
-            Assert.Equal(new[] { "a", "m", "b", "l", "a", "t" }, derived[0].Phonemes);
+            Assert.Equal(testData.PhonologicalForms.Length, derived.Length);
+
+            for (int i = 0; i < testData.PhonologicalForms.Length; i++)
+            {
+                var expected = testData.PhonologicalForms[i];
+                var real = derived[i];
+
+                Assert.Equal(expected.Phonemes, string.Join(string.Empty, real.Phonemes));
+                Assert.Equal(expected.GraphicalForms.Length, real.GraphicalForms.Length);
+
+                for (int j = 0; j < expected.GraphicalForms.Length; j++)
+                {
+                    var expectedG = expected.GraphicalForms[j];
+                    var realG = real.GraphicalForms[j];
+                    var realStr = string.Join(string.Empty, realG.Intervals.SelectMany(k => k.Value));
+
+                    Assert.Equal(expectedG, realStr);
+                }
+            }
+        }
+
+
+        private WordData ParseData(string[] data)
+        {
+            string latin = data[0];
+            var phono = data.Skip(1).ToArray();
+
+            var phonoData = new List<PhonologicalFormData>();
+            string phonemes = null;
+            var graphicalForms = new List<string>();
+
+            for (int i = 0; i < phono.Length; i++)
+            {
+                string segment = phono[i];
+                if (phonemes == null)
+                    phonemes = segment;
+                else if (segment == "||")
+                {
+                    phonoData.Add(new PhonologicalFormData(phonemes, graphicalForms.ToArray()));
+                    phonemes = null;
+                    graphicalForms.Clear();
+                }
+                else
+                    graphicalForms.Add(segment);
+            }
+            phonoData.Add(new PhonologicalFormData(phonemes, graphicalForms.ToArray()));
+
+            return new WordData(latin, phonoData.ToArray());
+        }
+    }
+
+    public class WordData
+    {
+        public string Latin { get; }
+        public PhonologicalFormData[] PhonologicalForms { get; }
+
+        public WordData(string latin, PhonologicalFormData[] phonologicalForms)
+        {
+            Latin = latin;
+            PhonologicalForms = phonologicalForms;
+        }
+    }
+
+    public class PhonologicalFormData
+    {
+        public string Phonemes { get; }
+        public string[] GraphicalForms { get; }
+
+        public PhonologicalFormData(string phonemes, string[] graphicalForms)
+        {
+            Phonemes = phonemes;
+            GraphicalForms = graphicalForms;
         }
     }
 }
