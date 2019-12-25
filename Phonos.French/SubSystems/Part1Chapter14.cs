089a9d116194b5d2ebd31743c54842de310d6502
mla <mla@proxem.com>
2019-12-25 11:47:28 +0100

    Add rules 2 for Part1Chapter14.

--- Phonos.French/SubSystems/Part1Chapter14.cs
+++ Phonos.French/SubSystems/Part1Chapter14.cs
@@ -20,7 +20,7 @@ namespace Phonos.French.SubSystems
             return new[]
             {
                 Rule1a(), Rule1b(), Rule1c(), Rule1d(), Rule1e(), Rule1f(), Rule1g(), Rule1h(),
-                //Rule2a(), Rule2b(), Rule2c(), Rule2d(), Rule2e(), Rule2f(), Rule2g(), Rule2h(), Rule2i(),
+                Rule2a(), Rule2b(), Rule2c(), Rule2d(), Rule2e(), Rule2f(), Rule2g(), Rule2h(), Rule2i(),
                 //Rule3a(), Rule3b(),
                 //Rule4a(), Rule4b(), Rule4c(), Rule4d(), Rule4e(),
                 //Rule5a(), Rule5b(),
@@ -143,6 +143,136 @@ namespace Phonos.French.SubSystems
                 .Rules(r => r
                     .Named("Nasalisation de /o/ en syllabe ouverte")
                     .Phono(px => new[] { "õ" })));
+        }
+
+
+
+        public static Rule Rule2a()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2a")
+                .From(1050).To(1075)
+                .Query(q => q
+                    .Scope("syllable")
+                    .Match(m => m.Phon("ẽ"))
+                    .After(a => a.Phon(p => !IPA.IsVowel(p))))
+                .Rules(r => r
+                    .Named("Ouverture de /ẽ/ en syllabe fermée")
+                    .Phono(px => new[] { "ɛ̃" })));
+        }
+
+        public static Rule Rule2b()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2b")
+                .From(1075).To(1100)
+                .Query(q => q
+                    .Scope("syllable")
+                    .Match(m => m.Phon("ɛ̃"))
+                    .After(a => a.Phon(p => !IPA.IsVowel(p))))
+                .Rules(r => r
+                    .Named("Ouverture de /ɛ̃/ en syllabe fermée")
+                    .Phono(px => new[] { "ã" })));
+        }
+
+        public static Rule Rule2c()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2c")
+                .From(1200).To(1300)
+                .Query(q => q
+                    .Scope("syllable")
+                    .Match(m => m.Phon("õ"))
+                    .After(a => a.Phon(p => !IPA.IsVowel(p))))
+                .Rules(r => r
+                    .Named("Ouverture de /õ/ en syllabe fermée")
+                    .Phono(px => new[] { "ɔ̃" })));
+        }
+
+        public static Rule Rule2d()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2d")
+                .From(1300).To(1350)
+                .Query(q => q
+                    .Scope("syllable")
+                    .Match(m => m.Phon("ĩ"))
+                    .After(a => a.Phon(p => !IPA.IsVowel(p))))
+                .Rules(r => r
+                    .Named("Ouverture de /ĩ/ en syllabe fermée")
+                    .Phono(px => new[] { "ẽ" })));
+        }
+
+        public static Rule Rule2e()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2e")
+                .From(1350).To(1400)
+                .Query(q => q
+                    .Scope("syllable")
+                    .Match(m => m.Phon("ẽ"))
+                    .After(a => a.Phon(p => !IPA.IsVowel(p))))
+                .Rules(r => r
+                    .Named("Ouverture de /ẽ/ en syllabe fermée")
+                    .Phono(px => new[] { "ɛ̃" })));
+        }
+
+        public static Rule Rule2f()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2f")
+                .From(1400).To(1450)
+                .Query(q => q
+                    .Scope("syllable")
+                    .Match(m => m.Phon("ỹ"))
+                    .After(a => a.Phon(p => !IPA.IsVowel(p))))
+                .Rules(r => r
+                    .Named("Ouverture de /ỹ/ en syllabe fermée")
+                    .Phono(px => new[] { "ø̃" })));
+        }
+
+        public static Rule Rule2g()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2g")
+                .From(1450).To(1500)
+                .Query(q => q
+                    .Scope("syllable")
+                    .Match(m => m.Phon("ø̃"))
+                    .After(a => a.Phon(p => !IPA.IsVowel(p))))
+                .Rules(r => r
+                    .Named("Ouverture de /ø̃/ en syllabe fermée")
+                    .Phono(px => new[] { "œ̃" })));
+        }
+
+        public static Rule Rule2h()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2h")
+                .From(1050).To(1075)
+                .Query(q => q
+                    .Match(m => m.Phon("ẽ"))
+                    .After(a => a.Seq(
+                        s => s.Phon(p => !IPA.IsVowel(p)),
+                        s => s.Phon(IPA.IsVowel))))
+                .Rules(r => r
+                    .Named("Ouverture de /ẽ/ en syllabe ouverte")
+                    .Phono(px => new[] { "ɛ̃" })));
+        }
+
+        public static Rule Rule2i()
+        {
+            return R.Rule(c => c
+                .Id("p1c14r2i")
+                .From(1200).To(1300)
+                .Query(q => q
+                    .Match(m => m.Phon("õ"))
+                    .After(a => a.Seq(
+                        s => s.Phon(p => !IPA.IsVowel(p)),
+                        s => s.Phon(IPA.IsVowel))))
+                .Rules(r => r
+                    .Named("Ouverture de /õ/ en syllabe ouverte")
+                    .Phono(px => new[] { "ɔ̃" })));
         }
     }
 }
