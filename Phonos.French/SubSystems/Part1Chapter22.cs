67452ce123e35303a72c5748668e95cccfb20670
mla <mla@proxem.com>
2020-01-02 17:54:12 +0100

    Add rule to handle aspirated greek consonants.

--- Phonos.French/SubSystems/Part1Chapter22.cs
+++ Phonos.French/SubSystems/Part1Chapter22.cs
@@ -13,6 +13,7 @@ namespace Phonos.French.SubSystems
         {
             return new[]
             {
+                Rule1c(),     // Chute de /ʰ/ des consonnes grecques
                 RuleSet1(),   // Chute du /h/ latin
                 Rule1b(),     // Chute du /h/ germanique
             };
@@ -22,7 +23,7 @@ namespace Phonos.French.SubSystems
         {
             return new[]
             {
-                Rule1a(), Rule1b(),
+                Rule1a(), Rule1b(), Rule1c(),
                 RuleSet1(),
             };
         }
@@ -55,6 +56,20 @@ namespace Phonos.French.SubSystems
                 .Rules(r => r
                     .Named("Amuïssement de /h/ (germanique)")
                     .Phono(P.Erase)));
+        }
+
+
+        // @fixme Initiative personelle, datation?
+        public static Rule Rule1c()
+        {
+            return R.Rule(c => c
+                .Id("p1c22r1c")
+                .From(0).To(100)
+                .Query(q => q.Match(m => m.Phon(p => p.Contains("ʰ"))))
+                .Rules(r => r
+                    .Named("Amuïssement de /ʰ/")
+                    .Phono(px => new[] { px[0].Substring(0, 1) })
+                    .Rewrite(g => g.Substring(0, 1))));
         }
     }
 }
